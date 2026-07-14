83ecf88c910e3c20bf8f83a7ab73f464be0047cf
gdandsm64fan123 <110691219+gdandsm64fan123@users.noreply.github.com>
2026-07-14 12:05:58 -0300

    Add files via upload

---
+++ wipuniversalscript.lua
@@ -0,0 +1,82 @@
+local UserInputService = game:GetService("UserInputService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+
+local IsOnMobile = table.find({Enum.Platform.IOS, Enum.Platform.Android}, UserInputService:GetPlatform())
+
+-- load fluent
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+
+-- window creation
+local Window = Fluent:CreateWindow({
+    Title = "cube64's universal script",
+    SubTitle = "subtitle here",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(480, 260),
+    Acrylic = false,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
+
+-- Add tabs
+local Main = Window:AddTab({
+	Title = "Main",
+	Icon = "globe"
+})
+   
+-- Cobalt Button
+Main:AddButton({
+	Title = "Cobalt",
+	Description = "very popular spy",
+	Callback = function()
+		loadstring(game:HttpGet("https://github.com/notpoiu/cobalt/releases/latest/download/Cobalt.luau"))()
+	end
+})
+
+-- brainrot police
+Main:AddButton({
+	Title = "Brainrot Police (multi-game support)",
+	Description = "by Vaehz (hes so cool gng",
+	Callback = function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/IcantAffordSynapse/BrainrotPolice/refs/heads/main/src/init.lua"))()
+	end
+})
+-- IY (even tho its in delta by default)
+Main:AddButton({
+	Title = "Infinite Yield",
+	Description = "a true classic of exploits",
+	Callback = function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/DarkNetworks/Infinite-Yield/main/latest.lua'))()
+	end
+})
+-- Dex
+Main:AddButton({
+	Title = "DexExplorer",
+	Description = "the inspect element of roblox",
+	Callback = function()
+		loadstring(game:HttpGet("https://obj.wearedevs.net/2/scripts/Dex%20Explorer.lua"))()
+	end
+})
+-- load notif
+Fluent:Notify({
+    Title = "script successfully loaded",
+    Content = "all systems go",
+    Duration = 20
+})
+
+-- saveman and interfaceman stuff
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({})
+
+SaveManager:SetFolder("speedclickhax")
+InterfaceManager:SetFolder("speedclickhax")
+
+SaveManager:BuildConfigSection(Tabs.Settings)
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+
+Window:SelectTab(1)
